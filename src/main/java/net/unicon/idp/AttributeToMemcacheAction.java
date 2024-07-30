88654ef9b9deced446eecc1743b06ba613a911b1
Andrew Tillinghast <atilling@ atillinghast-2709.local>
2024-07-30 09:11:40 -0400

    Added code to accept confguation from xml

--- src/main/java/net/unicon/idp/AttributeToMemcacheAction.java
+++ src/main/java/net/unicon/idp/AttributeToMemcacheAction.java
@@ -3,13 +3,18 @@ package net.unicon.idp;
 import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
+
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 import org.springframework.webflow.execution.Action;
-import net.spy.memcached.MemcachedClient;
+
+import org.opensaml.storage.impl.memcached.MemcachedStorageService;
+
 import net.shibboleth.idp.authn.context.SubjectContext;
 import net.shibboleth.idp.authn.principal.IdPAttributePrincipal;
 import net.shibboleth.idp.attribute.IdPAttribute;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.InetSocketAddress;
 
@@ -21,18 +26,30 @@ import java.security.Principal;
 
 import javax.security.auth.Subject;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AttributeToMemcacheAction implements Action {
+	
+	private static final Logger log = LoggerFactory.getLogger(AttributeToMemcacheAction.class);
 
-    private final String memcacheHost="127.0.0.1";
-    private final int memcachePort=11211;
-    private MemcachedClient memcachedClient;
+    private MemcachedStorageService memcachedStorageService;
+    private final ObjectMapper objectMapper;
+    private String keyName;
 
-    public AttributeToMemcacheAction() throws Exception {
-        memcachedClient = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
+    public AttributeToMemcacheAction(MemcachedStorageService memcachedStorageService, ObjectMapper objectMapper, String keyName) throws Exception {
+    	this.memcachedStorageService = memcachedStorageService;
+        this.objectMapper = objectMapper;
+    	this.keyName = keyName;
     }
 
     @Override
     public Event execute(RequestContext springRequestContext) {
+    	
+    	log.debug ("Starting save of attributes");
+    	
+    	String idKey = "default";
     	
     	Map<String, Object> attributes=new HashMap();
     	
@@ -42,12 +59,20 @@ public class AttributeToMemcacheAction implements Action {
         Set<IdPAttributePrincipal> principals = subject.getPrincipals(IdPAttributePrincipal.class);
         for (IdPAttributePrincipal principal:principals) {
         	IdPAttribute attribute = principal.getAttribute();
+        	log.debug("Add attribute: "+ attribute.getId());
+        	if (attribute.getId().equals(keyName)) {
+        		idKey=attribute.getValues().get(0).toString();
+        	}
         	attributes.put(attribute.getId(),attribute.getValues());
         }
         
 
         try {
-            memcachedClient.set("userAttributes", 3600, attributes);
+        	
+        	String jsonString = objectMapper.writeValueAsString(attributes);
+        	 
+        	memcachedStorageService.create("Attribs",idKey,jsonString,(long)3600);
+        	
         } catch (Exception e) {
             return new Event(this, "fail");
         }
